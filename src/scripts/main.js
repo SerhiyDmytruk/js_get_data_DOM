7f924eb0e9222028ac9fe20e13b64852c3fd3b43
Dmytruk Serhii <dmitruksl92@gmail.com>
2025-10-27 13:01:11 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write your code here
+const countries = document.querySelectorAll('.list__item .population');
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+let res = 0;
+
+countries.forEach((country) => {
+  res += country.innerText.replaceAll(',', '') * 1;
+});
+
+function numberFormatter(number) {
+  return number.toLocaleString();
+}
+
+total.innerText = numberFormatter(res);
+average.innerText = numberFormatter(Math.floor(res / countries.length));
